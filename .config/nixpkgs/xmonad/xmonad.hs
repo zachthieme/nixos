72fdb8b6726041d1c69ec68c66e4a63bfe9aef97
zachthieme <zach@techsage.org>
2022-05-03 11:38:06 -0700

    added three colum as the default for xmonad and removed window zooming

--- .config/nixpkgs/xmonad/xmonad.hs
+++ .config/nixpkgs/xmonad/xmonad.hs
@@ -30,10 +30,11 @@ myConfig = def
     ]
 -- TODO: add customization for when in ipad screen 
 --   < dimensions:    1366x980 pixels (361x259 millimeters)
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+myLayout = threeCol ||| tiled ||| Full 
   where
-    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+    threeCol = ThreeColMid nmaster delta ratio
+--    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
     tiled    = Tall nmaster delta ratio
     nmaster  = 1      -- Default number of windows in the master pane
-    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    ratio    = 1/3    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
