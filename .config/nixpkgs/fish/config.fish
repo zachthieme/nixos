40f90da5f87eb89260e6e46adf850420f069382c
zachthieme <zach@techsage.org>
2022-05-06 16:51:25 -0700

    added some new configs to fish

--- .config/nixpkgs/fish/config.fish
+++ .config/nixpkgs/fish/config.fish
@@ -1,3 +1,9 @@
+set -x STARSHIP_CONFIG $HOME/.config/starship.toml
+set -x HELIX_RUNTIME $HOME/.config/helix/runtime
+
+set -g fish_term24bit 1
+set -g theme_nerd_fonts yes
+
 # setup autojump for fish shell
  begin
      set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
@@ -24,4 +30,8 @@ alias cc='cp /etc/nixos/configuration.nix ~/.config/nixpkgs/system/configuration
 
 # alias nf='fdfind . /home/zach/.config -H --changed-within 1d -E Code -E chromium -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | sort | uniq'
 # alias nfa='fdfind . /home/zach/.config -H --changed-within 1d -E chromium -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | uniq | rargs /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add {0}'
+
+
+
+
 
